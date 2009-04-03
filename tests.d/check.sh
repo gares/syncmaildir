7c6cfe2c13f84c8d2e1236d64385cdd233eb0159
Enrico Tassi <tassi@cs.unibo.it>
2009-04-03 13:02:13 +0200

    better output message

--- tests.d/check.sh
+++ tests.d/check.sh
@@ -5,6 +5,6 @@ grep TRACE test.[0-9]*/log.client* | sort -u
 PATHS=`grep TRACE test.[0-9]*/log.client* | cut -d '|' -f 1 | cut -d : -f 2- | sort -u | wc -l`
 TRACES=`grep trace smd-client | grep -v ^function | wc -l`
 
-echo "Tested $PATHS out of $TRACES"
+echo "Tested $PATHS code paths out of $TRACES"
 
 
