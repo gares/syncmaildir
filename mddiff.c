6dd254b859bdb9cc2898071877ac5b078d771474
Enrico Tassi <tassi@cs.unibo.it>
2009-04-15 17:54:02 +0200

    touching a file updates its mtime in the db, no other actions

--- mddiff.c
+++ mddiff.c
@@ -389,8 +389,9 @@ void analize_file(const char* dir,const char* file) {
 	if (alias != NULL) {
 		if(sha_equal(alias->bsha,m->bsha)) {
 			if (sha_equal(alias->hsha, m->hsha)) {
-				alias->seen = m->seen = SEEN;
-				return;
+				alias->seen = SEEN;
+				alias->mtime = m->mtime;
+				goto err_alloc_fd_cleanup;
 			} else {
 				COMMAND_REPLACE_HEADER(alias,m);
 				m->seen=SEEN;
