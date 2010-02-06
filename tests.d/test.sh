387ffa8144dc1831ac5ed1af964331fa70667bf1
Enrico Tassi <gares@fettunta.org>
2010-02-06 19:05:33 +0100

    first working sample of pull test

--- tests.d/test.sh
+++ tests.d/test.sh
@@ -7,7 +7,7 @@ BASE=$PWD
 
 for TESTSUITE in $SUITES; do
 	run_tests $@
-	if [ -x $BASE/tests.d/$TESTSUITE/summarize.sh ]; then
+	if [ -x $BASE/tests.d/$TESTSUITE/summarize.sh -a -z "$1" ]; then
 		BASE=$BASE $BASE/tests.d/$TESTSUITE/summarize.sh
 	fi
 done
