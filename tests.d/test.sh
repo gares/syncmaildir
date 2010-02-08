2ba83d41645c65f35f5d25a858bf94e676c236fd
Enrico Tassi <gares@fettunta.org>
2010-02-08 19:22:19 +0100

    run summary after tests

--- tests.d/test.sh
+++ tests.d/test.sh
@@ -7,6 +7,8 @@ BASE=$PWD
 
 for TESTSUITE in $SUITES; do
 	run_tests $@
+done
+for TESTSUITE in $SUITES; do
 	if [ -x $BASE/tests.d/$TESTSUITE/summarize.sh -a -z "$1" ]; then
 		BASE=$BASE $BASE/tests.d/$TESTSUITE/summarize.sh
 	fi
